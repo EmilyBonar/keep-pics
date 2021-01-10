182f70aeab7a2e1b809c542b34db1e6c3315b565
emilybonar <emilykbonar@gmail.com>
2021-01-10 13:21:58 -0600

    Added basic home screen

---
+++ components/NavBar.js
@@ -0,0 +1,7 @@
+export default function NavBar(props) {
+	return (
+		<nav className="absolute top-0 flex text-white">
+			<button>Log In</button>
+		</nav>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Head from "next/head";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function Home() {
 	return (
-		<div className="">
+		<div className="flex flex-col text-white">
 			<Head>
 				<title>Create Next App</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<NavBar />
+			<main className="grid content-center justify-center w-5/6 h-screen m-auto">
+				<p className="text-5xl font-bold capitalize">Upload anonymously</p>
+			</main>
+			<Footer />
 		</div>
 	);
 }
