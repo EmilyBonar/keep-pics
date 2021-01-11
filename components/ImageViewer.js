3b9a8539c4cace3eea3ef9c2b6ab65ff57195822
emilybonar <emilykbonar@gmail.com>
2021-01-11 07:06:11 -0600

    Added Dimensions

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,15 +1,35 @@
 import { useEffect, useState } from "react";
 
 export default function ImageViewer(props) {
-	const [file, setFile] = useState("");
-
-	useEffect(() => {
-		setFile(props.file);
-	});
-
+	const file = URL.createObjectURL(props.image);
+	const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+	const [imageLoaded, setImageLoaded] = useState(false);
 	return (
-		<div>
-			<img src={file}></img>
+		<div className="flex gap-2">
+			<img
+				src={file}
+				className="max-w-lg border-2 border-black rounded max-h-lg"
+				onLoad={(e) => {
+					if (!imageLoaded) {
+						setDimensions({
+							width: e.target.naturalWidth,
+							height: e.target.naturalHeight,
+						});
+						URL.revokeObjectURL(file);
+					}
+					setImageLoaded(true);
+				}}
+			></img>
+			<div className="text-gray-200">
+				<p>Name: {props.image.name}</p>
+				<p>Size: {(props.image.size / 1024).toFixed(2)} KB</p>
+				<p>
+					Last Modified: {new Date(props.image.lastModified).toLocaleString()}
+				</p>
+				<p>
+					Dimensions: {dimensions.width}x{dimensions.height}
+				</p>
+			</div>
 		</div>
 	);
 }
