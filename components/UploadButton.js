484450d0d832b354ccc09b31e982dcc8583ffb7e
emilybonar <emilykbonar@gmail.com>
2021-01-11 09:02:33 -0600

    Adjusted button

--- components/UploadButton.js
+++ components/UploadButton.js
@@ -12,7 +12,7 @@ export default function UploadButton(props) {
 				className="absolute w-1 h-1 opacity-0"
 				onChange={(e) => props.handleFiles(e)}
 			/>
-			<p className="p-2 m-auto border-2 border-white rounded hover:bg-gray-800 w-min ">
+			<p className="p-2 m-auto tracking-wide uppercase rounded-lg hover:bg-gray-900 w-min ring-white ring-1 hover:ring-2 hover:font-semibold">
 				<label htmlFor="file">Upload</label>
 			</p>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
 		extend: {},
 	},
 	variants: {
-		extend: {},
+		extend: { ringWidth: ["hover"], fontWeight: ["hover", "focus"] },
 	},
 	plugins: [],
 };
