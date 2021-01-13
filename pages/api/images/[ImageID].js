9c19aadf324dce424c91207f348a7c64e926856c
emilybonar <emilykbonar@gmail.com>
2021-01-13 10:39:36 -0600

    Set up API endpoints

--- pages/api/images/[ImageID].js
+++ pages/api/images/[ImageID].js
@@ -1,17 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import admin from "./firebaseConnect";
+import admin from "./../firebaseConnect";
 
 export default async function handle(req, res) {
 	var db = admin.firestore();
 
 	let ret = [];
-
+	/*
 	let querySnapshot = await db.collection("images").get();
 
 	querySnapshot.forEach((doc) => {
 		ret.push(doc.data());
-	});
+    });
+    */
 
 	res.statusCode = 200;
 	res.json(ret);
---
+++ pages/api/users/create.js
@@ -0,0 +1,17 @@
+import admin from "./../firebaseConnect";
+
+export default async function handle(req, res) {
+	var db = admin.firestore();
+
+	let ret = [];
+	/*
+	let querySnapshot = await db.collection("images").get();
+
+	querySnapshot.forEach((doc) => {
+		ret.push(doc.data());
+    });
+    */
+
+	res.statusCode = 200;
+	res.json(ret);
+}
