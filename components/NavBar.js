36c9775088e47b43e3f7954096f5c9bc1d096b2a
emilybonar <emilykbonar@gmail.com>
2021-01-14 17:44:01 -0600

    Added auth

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,12 +1,41 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import netlifyAuth from "../config/netlifyAuth";
 
 export default function NavBar(props) {
+	let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+	let [user, setUser] = useState(null);
+
+	useEffect(() => {
+		netlifyAuth.initialize((user) => {
+			setLoggedIn(!!user);
+		});
+	}, [loggedIn]);
+
+	let login = () => {
+		netlifyAuth.authenticate((user) => {
+			setLoggedIn(!!user);
+			setUser(user);
+			netlifyAuth.closeModal();
+		});
+	};
+
+	let logout = () => {
+		netlifyAuth.signout(() => {
+			setLoggedIn(false);
+			setUser(null);
+		});
+	};
+
+	console.log(loggedIn);
 	return (
 		<div className="w-full">
 			<nav className="flex ">
 				<Logo clearImage={props.clearImage} />
 				<span className="flex-grow"></span>
-				<button className="p-2 text-xl">Log In</button>
+				<button className="p-2 text-xl" onClick={login}>
+					Log In
+				</button>
 			</nav>
 		</div>
 	);
